d68d4cb1c511969aad39b51ded89357eca9696c0
astillo <clomes.alex@gmail.com>
2019-09-10 12:44:36 -0500

    getting ready to migrate db

--- data/migrations/20190910123008_bootstrap.js
+++ data/migrations/20190910123008_bootstrap.js
@@ -1,6 +1,15 @@
 //changes we want to make
 exports.up = function (knex) {
-
+    //here is where we create the table with all of the attributes from the car.
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increments()
+        tbl.string('VIN', 500).unique().notNullable();
+        tbl.string('make', 128).notNullable();
+        tbl.string('model', 128).notNullable();
+        tbl.string('mileage', 128).notNullable();
+        tbl.string('transmission_type', 128);
+        tbl.string('title_status', 128)
+    })
 };
 //revert the changes
 exports.down = function (knex) {
