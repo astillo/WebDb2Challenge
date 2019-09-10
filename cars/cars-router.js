5adcf373c3c1c78e762875385351847767bd40e7
astillo <clomes.alex@gmail.com>
2019-09-10 12:27:38 -0500

    added routes

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -5,3 +5,12 @@ const db = require('../data/db-config')
 
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    db('cars')
+        .then(cars => {
+            res.json(cars);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to retrieve cars' });
+        });
+});
